108ed7d0537f629a633057187bc25b93e4361b35
Info Aakvatech <info@aakvatech.com>
2021-01-07 08:50:48 +0000

    fix: when migrating to new system with same key, the registration_doc.gc needs to be made int() and then subtracted from

--- vfd_tz/vfd_tz/doctype/vfd_uin/vfd_uin.py
+++ vfd_tz/vfd_tz/doctype/vfd_uin/vfd_uin.py
@@ -9,35 +9,36 @@ from frappe.utils import nowdate, getdate
 from frappe import _
 from api.utlis import get_latest_registration_doc
 
+
 class VFDUIN(Document):
-	def on_trash(self):
-		frappe.throw(_("This document cannot be deleted"))
+    def on_trash(self):
+        frappe.throw(_("This document cannot be deleted"))
 
 
 def get_counters(company):
-	doc = ""
-	if not frappe.db.exists("VFD UIN", company):
-		registration_doc = get_latest_registration_doc(company)
-		gc = registration_doc.gc or 1
-		doc = frappe.get_doc({
-			"doctype" : "VFD UIN",
-			"company": company,
-			"gc": gc - 1,
-			"dc": 0,
-			"dc_date" : nowdate()
-		})
-		doc.insert(ignore_permissions=True)
-		doc.save(ignore_permissions=True)
-		doc.reload()
-	else:
-		doc = frappe.get_doc("VFD UIN", company)
-	
-	if doc.dc_date < getdate():
-		doc.dc_date = nowdate()
-		doc.dc = 0
-	
-	doc.dc += 1
-	doc.gc += 1
-	doc.save(ignore_permissions=True)
-	doc.reload()
-	return doc
+    doc = ""
+    if not frappe.db.exists("VFD UIN", company):
+        registration_doc = get_latest_registration_doc(company)
+        gc = int(registration_doc.gc) or 1
+        doc = frappe.get_doc({
+            "doctype": "VFD UIN",
+            "company": company,
+            "gc": gc - 1,
+            "dc": 0,
+            "dc_date": nowdate()
+        })
+        doc.insert(ignore_permissions=True)
+        doc.save(ignore_permissions=True)
+        doc.reload()
+    else:
+        doc = frappe.get_doc("VFD UIN", company)
+
+    if doc.dc_date < getdate():
+        doc.dc_date = nowdate()
+        doc.dc = 0
+
+    doc.dc += 1
+    doc.gc += 1
+    doc.save(ignore_permissions=True)
+    doc.reload()
+    return doc
