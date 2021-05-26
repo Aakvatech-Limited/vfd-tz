520c6391ff594f250b2ce17b1949b663ec42de9e
Info Aakvatech <info@aakvatech.com>
2021-05-26 16:38:55 +0000

    feat: VFD Z Report doctypes

---
+++ vfd_tz/vfd_tz/doctype/vfd_z_report/test_vfd_z_report.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, Aakvatech and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestVFDZReport(unittest.TestCase):
+	pass
---
+++ vfd_tz/vfd_tz/doctype/vfd_z_report/vfd_z_report.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Aakvatech and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('VFD Z Report', {
+	// refresh: function(frm) {
+
+	// }
+});
