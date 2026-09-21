0c2eda4da39932a33617466d1b4de0f56d8b64d4
av-dev2 <emgani@aakvatech.com>
2026-09-21 10:23:07 +0300

    style: apply ruff formatting

--- vfd_tz/patches/update_sales_invoice_vfd_serial.py
+++ vfd_tz/patches/update_sales_invoice_vfd_serial.py
@@ -1,29 +1,23 @@
-from __future__ import unicode_literals
 import frappe
+
 from vfd_tz.api.utils import get_latest_registration_doc
 
 
 def execute():
-    invoices_list = frappe.get_all(
-        "Sales Invoice",
-        filters={"docstatus": 1, "vfd_gc": [">", 0], "vfd_serial": ["in", ["", None]]},
-        fields=["name", "company"],
-    )
-    registrations_dict = frappe._dict()
-    for invoice in invoices_list:
-        registration_doc: None
-        if not registrations_dict.get(invoice.company):
-            registration_doc = get_latest_registration_doc(invoice.company)
-            registrations_dict[invoice.company] = registration_doc
-        else:
-            registration_doc = registrations_dict.get(invoice.company)
-        if not registration_doc:
-            continue
-        frappe.db.set_value(
-            "Sales Invoice", invoice.name, "vfd_serial", registration_doc.serial
-        )
-        print(
-            "Updated Sales Invoice {0} with VFD Serial {1}".format(
-                invoice.name, registration_doc.serial
-            )
-        )
+	invoices_list = frappe.get_all(
+		"Sales Invoice",
+		filters={"docstatus": 1, "vfd_gc": [">", 0], "vfd_serial": ["in", ["", None]]},
+		fields=["name", "company"],
+	)
+	registrations_dict = frappe._dict()
+	for invoice in invoices_list:
+		registration_doc: None
+		if not registrations_dict.get(invoice.company):
+			registration_doc = get_latest_registration_doc(invoice.company)
+			registrations_dict[invoice.company] = registration_doc
+		else:
+			registration_doc = registrations_dict.get(invoice.company)
+		if not registration_doc:
+			continue
+		frappe.db.set_value("Sales Invoice", invoice.name, "vfd_serial", registration_doc.serial)
+		print(f"Updated Sales Invoice {invoice.name} with VFD Serial {registration_doc.serial}")
