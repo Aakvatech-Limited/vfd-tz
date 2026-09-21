0c001b4f4a2ecf05f091fb20a6cb7934765ca93a
av-dev2 <emgani@aakvatech.com>
2026-09-21 11:12:13 +0300

    style: apply prettier formatting

--- vfd_tz/vfd_tz/doctype/vfd_registration/vfd_registration.js
+++ vfd_tz/vfd_tz/doctype/vfd_registration/vfd_registration.js
@@ -1,22 +1,22 @@
 // Copyright (c) 2020, Aakvatech and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('VFD Registration', {
-	refresh: function (frm) {
-		frm.add_custom_button(__('Get new token'), function () {
-			frm.trigger("get_new_token");
-		});
-	},
-	get_new_token: function (frm) {
-		frappe.call({
-			method: 'vfd_tz.vfd_tz.doctype.vfd_token.vfd_token.get_token',
-			args: {
-				'company': frm.doc.company,
-				'force': 1,
-			},
-			callback: function (r) {
-				console.log(r);
-			}
-		});
-	}
+frappe.ui.form.on("VFD Registration", {
+  refresh: function (frm) {
+    frm.add_custom_button(__("Get new token"), function () {
+      frm.trigger("get_new_token");
+    });
+  },
+  get_new_token: function (frm) {
+    frappe.call({
+      method: "vfd_tz.vfd_tz.doctype.vfd_token.vfd_token.get_token",
+      args: {
+        company: frm.doc.company,
+        force: 1,
+      },
+      callback: function (r) {
+        console.log(r);
+      },
+    });
+  },
 });
